9d01fd2f539475347803b817fb2a35af00b5d205
hao.phan <hao.phan@sotatek.com>
2021-08-28 14:43:14 +0700

    End of section 6

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { SyntheticEvent } from 'react';
+import { useState } from 'react';
 import { Button, Item, Label, Segment } from 'semantic-ui-react';
 import { Activity } from '../../../app/models/activity';
 
@@ -6,9 +7,25 @@ interface Props {
    activities: Activity[];
    selectActivity: (id: string) => void;
    deleteActivity: (id: string) => void;
+   submitting: boolean;
 }
 
-function ActivityList({ activities, selectActivity, deleteActivity }: Props) {
+function ActivityList({
+   activities,
+   selectActivity,
+   deleteActivity,
+   submitting,
+}: Props) {
+   const [target, setTarget] = useState('');
+
+   const handleActivityDelete = (
+      e: SyntheticEvent<HTMLButtonElement>,
+      id: string
+   ) => {
+      setTarget(e.currentTarget.name);
+      deleteActivity(id);
+   };
+
    return (
       <Segment>
          <Item.Group divided>
@@ -31,7 +48,9 @@ function ActivityList({ activities, selectActivity, deleteActivity }: Props) {
                            color='blue'
                         />
                         <Button
-                           onClick={() => deleteActivity(activity.id)}
+                           name={activity.id}
+                           loading={submitting && target === activity.id}
+                           onClick={(e) => handleActivityDelete(e, activity.id)}
                            floated='right'
                            content='Delete'
                            color='red'
